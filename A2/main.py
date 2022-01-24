730a235c2cd8e7c86f09f5cb27d3d85d0385ca17
JanRob23 <jan.henner.roberg@gmail.com>
2022-01-24 16:43:36 +0100

    added atability plot

--- A2/main.py
+++ A2/main.py
@@ -54,7 +54,7 @@ def train(x, y, max_epochs):
         if stopping_criterion(prev_stability, k[i]):
             break
         prev_stability = k[i]
-    return w
+    return w, k
 
 def make_plot(x, ys, save=False, title='Generalisation Error of Algorithm', label_desc = 'N: '):
     plt.figure()
@@ -73,6 +73,21 @@ def calc_generalisation_error(w, w_teacher):
     calc = np.dot(w, w_teacher)/ (np.linalg.norm(w) * np.linalg.norm(w_teacher))
     return 1 / math.pi * np.arccos(calc)
 
+def plot_stabilities(stabilities, title='Distribution of stabilities', save= False):
+    hist_arr = []
+    for ks in stabilities:
+        for k in ks:
+            hist_arr.append(k[0])
+    plt.figure()
+    plt.title(title)
+    plt.xlabel('K, Distance from the hyperplane')
+    plt.ylabel('Amount of data points')
+    #plt.legend()
+    plt.hist(hist_arr, bins=30, range=(0, 20))
+    if save:
+        plt.savefig(f'plots/A2_plots/{title}.jpg')
+    plt.show()
+
 def main():
     total_datasets = 20
     all_errors = {}
@@ -80,19 +95,24 @@ def main():
     Ns = [5, 50, 100]
     epoch_sizes = [300]
     alphas = np.arange(0.25, 5.01, 0.25)
+    
     for max_epochs in tqdm(epoch_sizes, desc='running different sizes of N', leave=False):
         for N in tqdm(Ns, desc='running different sizes of N', leave=False):
             errors = []
+            stabilities = []
             for alpha in tqdm(alphas, desc='running different alphas', leave=False):
                 P = int(alpha * N)
                 generalisation = 0
                 for n in tqdm(range(0, total_datasets), desc='training perceptron on different datasets', leave=False):
                     x_data, y_data, w_teacher = generate_random_data(P, N)
-                    w_max = train(x_data, y_data, max_epochs)
+                    w_max, k = train(x_data, y_data, max_epochs)
                     generalisation += calc_generalisation_error(w_max, w_teacher)
+                    if alpha == 5:
+                        stabilities.append(k)
                 average_generalisation_error = generalisation / total_datasets
                 errors.append(average_generalisation_error)
             all_errors[N] = errors
+    plot_stabilities(stabilities, save=True)
     make_plot(alphas, all_errors, save=True)
 
 
